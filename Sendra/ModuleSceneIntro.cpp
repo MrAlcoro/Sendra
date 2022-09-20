8ba7b83276d5c2ac35d4c5a73b687e4f220a6b94
MrAlcoro <alcoro13@gmail.com>
2022-09-20 20:54:54 +0200

    ImGui's integration

--- Sendra/ModuleSceneIntro.cpp
+++ Sendra/ModuleSceneIntro.cpp
@@ -3,6 +3,11 @@
 #include "ModuleSceneIntro.h"
 #include "Primitive.h"
 
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_opengl3.h"
+#include "ImGui/imgui_impl_sdl.h"
+
+
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
 }
@@ -16,6 +21,13 @@ bool ModuleSceneIntro::Start()
 	LOG("Loading Intro assets");
 	bool ret = true;
 
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplSDL2_InitForOpenGL(App->window->window, NULL);
+	ImGui_ImplOpenGL3_Init();
+
 	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
 	return ret;
@@ -25,6 +37,12 @@ bool ModuleSceneIntro::Start()
 bool ModuleSceneIntro::CleanUp()
 {
 	LOG("Unloading Intro scene");
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplSDL2_Shutdown();
+	ImGui::DestroyContext();
+	SDL_DestroyWindow(App->window->window);
+	SDL_Quit();
 
 	return true;
 }
